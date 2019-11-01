f5bd49c22494d1db91c70c0142cbf632b81d0738
SunFlow <sunflow.riven@gmail.com>
2019-11-01 02:23:56 +0100

    10.11: Neural Networks: Matrix Class Improvements - The Nature of Code

---
+++ SimpleNeuralNetwork/src/com/sunflow/simpleneuralnetwork/NeuralNetwork.java
@@ -0,0 +1,18 @@
+package com.sunflow.simpleneuralnetwork;
+
+public class NeuralNetwork {
+
+	private int nodes_input;
+	private int nodes_hidden;
+	private int nodes_output;
+
+	public NeuralNetwork(int nodes_input, int nodes_hidden, int nodes_output) {
+		this.nodes_input = nodes_input;
+		this.nodes_hidden = nodes_hidden;
+		this.nodes_output = nodes_output;
+	}
+
+	public void feedforward(float input) {
+
+	}
+}
--- SimpleNeuralNetwork/src/com/sunflow/simpleneuralnetwork/SimpleNeuralNetwork.java
+++ SimpleNeuralNetwork/src/com/sunflow/simpleneuralnetwork/SimpleNeuralNetwork.java
@@ -1,5 +1,41 @@
 package com.sunflow.simpleneuralnetwork;
 
-public class SimpleNeuralNetwork {
+import java.awt.Graphics2D;
 
+import com.sunflow.game.Game2D;
+import com.sunflow.math3d.MatrixF;
+import com.sunflow.math3d.MatrixF.Mapper;
+import com.sunflow.util.Log;
+
+public class SimpleNeuralNetwork extends Game2D {
+	public static void main(String[] args) {
+		new SimpleNeuralNetwork();
+	}
+
+	private NeuralNetwork brain;
+
+	@Override
+	protected void setup() {
+		createCanvas(400, 400);
+
+		brain = new NeuralNetwork(3, 3, 1);
+
+		MatrixF a = new MatrixF(2, 3);
+		a.randomize(10);
+		Log.info(a);
+
+		Mapper doubleIt = new Mapper() {
+			@Override
+			public float func(float x, int i, int j) {
+				return x * 2;
+			}
+		};
+		a.map(doubleIt);
+		Log.info(a);
+	}
+
+	@Override
+	protected void render(Graphics2D g) {
+		super.render(g);
+	}
 }
