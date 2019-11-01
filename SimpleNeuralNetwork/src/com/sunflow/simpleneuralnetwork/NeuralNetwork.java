8ca13f6012ee7ca0a47697cd4ff78a374a7f02db
SunFlow <sunflow.riven@gmail.com>
2019-11-01 03:22:00 +0100

    10.13: Neural Networks: Feedforward Algorithm Part 2 - The Nature of Code

--- SimpleNeuralNetwork/src/com/sunflow/simpleneuralnetwork/NeuralNetwork.java
+++ SimpleNeuralNetwork/src/com/sunflow/simpleneuralnetwork/NeuralNetwork.java
@@ -1,18 +1,70 @@
 package com.sunflow.simpleneuralnetwork;
 
+import com.sunflow.math3d.MatrixF;
+
 public class NeuralNetwork {
+	private float sigmoid(float x) {
+		return (float) (1 / (1 + Math.exp(-x)));
+	}
 
 	private int nodes_input;
 	private int nodes_hidden;
 	private int nodes_output;
 
+	private MatrixF weights_ih;
+	private MatrixF weights_ho;
+
+	private MatrixF bias_h;
+	private MatrixF bias_o;
+
 	public NeuralNetwork(int nodes_input, int nodes_hidden, int nodes_output) {
 		this.nodes_input = nodes_input;
 		this.nodes_hidden = nodes_hidden;
 		this.nodes_output = nodes_output;
+
+		this.weights_ih = new MatrixF(nodes_hidden, nodes_input);
+		this.weights_ho = new MatrixF(nodes_output, nodes_hidden);
+		this.weights_ih.randomize();
+		this.weights_ho.randomize();
+
+		this.bias_h = new MatrixF(nodes_hidden, 1);
+		this.bias_o = new MatrixF(nodes_output, 1);
+		this.bias_h.randomize();
+		this.bias_o.randomize();
 	}
 
-	public void feedforward(float input) {
+	public float[] feedforward(float[] input) {
+		MatrixF output = feedforward(MatrixF.fromArray(input));
+		return output.toArray();
+	}
 
+	public MatrixF feedforward(MatrixF input) {
+		// Generating the hidden outputs
+		MatrixF hidden = MatrixF.dot(weights_ih, input);
+		hidden.add(bias_h);
+		// Activation function
+		hidden.map((x, i, j) -> sigmoid(x));
+
+		// Generating the real outputs
+		MatrixF output = MatrixF.dot(weights_ho, hidden);
+		output.add(bias_o);
+		// Activation function
+		output.map((x, i, j) -> sigmoid(x));
+
+		// Sending back to the caller!
+		return output;
+
+//		MatrixF hidden = genLayer(weights_ih, input, bias_h);
+//		MatrixF output = genLayer(weights_ho, hidden, bias_o);
+//		return output;
+	}
+
+	private MatrixF genLayer(MatrixF weights, MatrixF input, MatrixF bias) {
+		// Generating the layer outputs
+		MatrixF layer = MatrixF.dot(weights, input);
+		layer.add(bias);
+		// Activation function
+		layer.map((x, i, j) -> sigmoid(x));
+		return layer;
 	}
 }
--- SimpleNeuralNetwork/src/com/sunflow/simpleneuralnetwork/SimpleNeuralNetwork.java
+++ SimpleNeuralNetwork/src/com/sunflow/simpleneuralnetwork/SimpleNeuralNetwork.java
@@ -3,8 +3,6 @@ package com.sunflow.simpleneuralnetwork;
 import java.awt.Graphics2D;
 
 import com.sunflow.game.Game2D;
-import com.sunflow.math3d.MatrixF;
-import com.sunflow.math3d.MatrixF.Mapper;
 import com.sunflow.util.Log;
 
 public class SimpleNeuralNetwork extends Game2D {
@@ -12,26 +10,18 @@ public class SimpleNeuralNetwork extends Game2D {
 		new SimpleNeuralNetwork();
 	}
 
-	private NeuralNetwork brain;
+//	private NeuralNetwork brain;
 
 	@Override
 	protected void setup() {
 		createCanvas(400, 400);
 
-		brain = new NeuralNetwork(3, 3, 1);
+//		brain = new NeuralNetwork(2, 2, 1);
 
-		MatrixF a = new MatrixF(2, 3);
-		a.randomize(10);
-		Log.info(a);
-
-		Mapper doubleIt = new Mapper() {
-			@Override
-			public float func(float x, int i, int j) {
-				return x * 2;
-			}
-		};
-		a.map(doubleIt);
-		Log.info(a);
+		NeuralNetwork nn = new NeuralNetwork(2, 2, 1);
+		float[] input = { 1, 0 };
+		float[] output = nn.feedforward(input);
+		Log.info(output[0]);
 	}
 
 	@Override
